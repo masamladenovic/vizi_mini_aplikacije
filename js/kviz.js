f4b45b2e3c9ccaa0b6fa03f0526c1d8995ba6ca3
masha <masamladenovic06@gmail.com>
2026-08-17 14:30:31 +0200

    Dodat tajmer na kviz

--- js/kviz.js
+++ js/kviz.js
@@ -18,29 +18,64 @@ const pitanja = [
 
 let indeksPitanja = 0;
 let poeni = 0;
+let vreme = 10;
+let interval;
 
 const pitanjeElement = document.getElementById("pitanje");
 const odgovoriElement = document.getElementById("odgovori");
 const dugmeDaljeElement = document.getElementById("dalje");
 const rezultatElement = document.getElementById("rezultat");
+const tajmerElement = document.getElementById("tajmer");
 
 function proveriOdgovor(odgovor, dugme) {
+  clearInterval(interval);
+  onemoguciOdgovore();
   if (odgovor == pitanja[indeksPitanja].tacan) {
     poeni++;
     dugme.style.backgroundColor = "green";
     dugmeDaljeElement.style.display = "block";
-      alert("Tačan odgovor!");
+    alert("Tačan odgovor!");
   } else {
     dugme.style.backgroundColor = "red";
     dugmeDaljeElement.style.display = "block";
-    alert("Netačan odgovor! Tačan odgovor je "+ pitanja[indeksPitanja].tacan);
+    alert("Netačan odgovor! Tačan odgovor je " + pitanja[indeksPitanja].tacan);
   }
+}
+
+function onemoguciOdgovore() {
+  const dugmici = odgovoriElement.querySelectorAll("#odgovori button");
+  dugmici.forEach((dugme) => {
+    dugme.disabled = true;
+  });
 }
 
 function prikaziPitanje() {
   dugmeDaljeElement.style.display = "none";
   pitanjeElement.innerHTML = pitanja[indeksPitanja].tekst;
   odgovoriElement.innerHTML = "";
+
+  vreme = 10;
+  tajmerElement.innerHTML = `Preostalo vreme: ${vreme}s`;
+
+  interval = setInterval(() => {
+    vreme--;
+    tajmerElement.innerHTML = `Preostalo vreme: ${vreme}s`;
+    if (vreme <= 3) {
+      tajmerElement.style.color = "red";
+    } else {
+      tajmerElement.style.color = "#333";
+    }
+
+    if (vreme === 0) {
+      clearInterval(interval);
+      onemoguciOdgovore();
+      alert(
+        `Vreme je isteklo! Tačan odgovor je ${pitanja[indeksPitanja].tacan}.`,
+      );
+      dugmeDaljeElement.style.display = "block";
+    }
+  }, 1000);
+
   pitanja[indeksPitanja].odgovori.forEach((odgovor) => {
     const dugme = document.createElement("button");
     dugme.innerHTML = odgovor;
@@ -55,6 +90,7 @@ function prikazRezultata() {
   pitanjeElement.style.display = "none";
   odgovoriElement.style.display = "none";
   dugmeDaljeElement.style.display = "none";
+  tajmerElement.style.display = "none";
   rezultatElement.style.display = "block";
   rezultatElement.innerHTML = `Osvojili ste ${poeni} poena.`;
 }
